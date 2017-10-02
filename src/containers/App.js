4b92688ce47aa5946fc904ad2653889b31815f39
Franklin Crosby <obleo33@gmail.com>
2017-10-02 16:44:26 -0600

    Add basic layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import logo from '../assets/logo.svg'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        HEY
+     <div>
+        <section className="hero">
+          <img className="hero-logo" src={logo} alt='mirrored background'/>
+        </section>
+        <section className="contact">
+          <h2>FRANKLIN CROSBY</h2>
+          <p>PHONE: 303-887-2413</p>
+          <p>EMAIL: obleo33@gmail.com</p>
+        </section>
       </div>
     );
   }
