47cb8e684c01fef8b54e92da916bcc536eaff111
Franklin Crosby <obleo33@gmail.com>
2017-10-12 16:20:31 -0600

    Added skills and projects section. Updated About section. Added new dropdown menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import logo from '../assets/logo_white2.svg'
 
-const Nav = () => {
+import menu from '../../src/assets/menu-hamburger-closed.svg'
+import menuOpen from '../../src/assets/menu-hamburger-open.svg'
+
+
+const menuStyle = (open) => open?
+	{backgroundImage: `url(${menuOpen})`}:
+	{backgroundImage: `url(${menu})`} 
+
+const Nav = (props) => {
 	return(
 		<nav className="nav">
 			<img className="nav-logo" src={logo} alt="small logo"/>
-			<ul className="nav-links">
+
+			<h1>FRANKLIN<span>CROSBY</span></h1>
+
+			{ props.viewMenu && <ul className="nav-links">
 				<li>HELLO</li>
 				<li>SKILLS</li>
-				<li>EXPERIENCE</li>
 				<li>PROJECTS</li>
 				<li>CONTACT</li>
-				<li>INTERESTS</li>
-			</ul>
+			</ul>}
+			<div onClick={ props.navClick } className="nav-button" style={menuStyle(props.viewMenu)}></div>
 		</nav>
 	)
 }
 
 export default Nav;
+				// <li>EXPERIENCE</li>
+				// <li>INTERESTS</li>
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,17 +5,41 @@ import logo from '../assets/logo.svg'
 import Nav from '../components/Nav'
 import About from '../components/About'
 import Skills from '../components/Skills'
+import Projects from '../components/Projects'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      viewMenu: false,
+    }
+  }
+
+  navClick() {
+    this.setState({ viewMenu: !this.state.viewMenu })
+  }
+
+  linkClick() {
+    console.log('hey');
+  }
+
+
   render() {
     return (
-     <div>
-        <Nav />
+     <div id="app">
+        <Nav viewMenu={ this.state.viewMenu } 
+             navClick={ this.navClick.bind(this) }
+             linkClick={ this.linkClick.bind(this) }/>
+
         <section className="hero">
           <img className="hero-logo" src={logo} alt='mirrored background'/>
         </section>
+
         <About />
+
         <Skills />
+
+        <Projects />
      
       </div>
     );
