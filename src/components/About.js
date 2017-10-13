641a5b4fa50f4f326628a42963b3ffa776c1f75c
Franklin Crosby <obleo33@gmail.com>
2017-10-13 14:49:47 -0600

    Edit copy

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,13 @@ const About = () => {
 	return(
 		<article className="about">
 			<h2 className="section-title about-title">HELLO!</h2>
-			<p>I am a Front-end developer with critical skills in communication, team building, design, ideation and management. As a developer, I am constantly learning and building upon my skills with the goal of delivering new and inventive ways to interact with users. My diverse background informs my design, bringing new ideas and strategies to the table.
-			</p> 
+			<p>I am a Front-end developer with critical skills in communication, team building, design, ideation and management. As a developer, I am constantly learning and building upon my skills with the goal of delivering new and inventive ways to interact with users. My diverse background informs my design, bringing new ideas and strategies to the table.</p> 
 
 			<h3>Why Development:</h3>
-			<p>Since a young age, I have been obsessed with puzzles, games and building. It’s safe to say I had an epic Lego collection. I love figuring out how things work and taking that knowledge to build new things. As a developer, I am constantly presented with new and challenging puzzles. It is my passion to draw from everything around me to fabricate a solution and work with others to bring ideas to life. 
-			</p>
+			<p>Since a young age, I have been obsessed with puzzles, games and building. It’s safe to say I had an epic Lego collection. I love figuring out how things work and taking that knowledge to build new things. As a developer, I am constantly presented with new and challenging puzzles. It is my passion to draw from everything around me to fabricate a solution and work with others to bring ideas to life.</p>
 
 			<h3>What do you geek out on:</h3>
-			<p>I totally geek out on science ficion, roll playing games, board games, web design, graphic design, industrial design, remote controll quadcopters, movies, comics, video game, my family, my friends and (best for last) my kids </p>
+			<p>I totally geek out on science ficion, roll playing games, board games, web design, graphic design, industrial design, remote controll quadcopters, movies, comics, video game, my family, my friends and (best for last) my kids.</p>
 
 		</article>
 	)
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,7 +14,7 @@ const Projects = () => {
 					</a>
 					<section>
 						<h3 className="project-title">JetFuel</h3>
-						<p className="project-desctiption">Jet Fuel is a URL shortening service. Users are able to create a login/ sign in/ create a folders/ add urls to be shortened. Folders and shortened urls are stored on the database.</p>
+						<p className="project-desctiption">Jet Fuel is a URL shortening service. Users are able to create a login/ sign in/ create folders/ add urls to be shortened. Folders and shortened urls are stored in a database.</p>
 						<div className="project-links">
 							<a href="https://jetfuel-.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a>
 							<a href="https://github.com/Obleo33/jet_fuel" target="_blank" rel="noopener noreferrer">GitHub</a>
@@ -28,7 +28,7 @@ const Projects = () => {
 						</a>
 					<section>
 						<h3 className="project-title">MovingShadows</h3>
-						<p className="project-desctiption">This project is working off the The Movie DB API . Users should be able to sign in and save favorite movies. Once a movie has been added to favorites the user should be able to navigate to a favorites page and remove it.</p>
+						<p className="project-desctiption">This project is working off the The Movie DB API . Users are able to sign in and save favorite movies. Once a movie has been added to favorites, the user is able to navigate to the favorites page and view all movies as well as remove them from the list.</p>
 						<div className="project-links">
 							<a href="https://movingshadows.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live</a>
 							<a href="https://github.com/Obleo33/movie-tracker" target="_blank" rel="noopener noreferrer">GitHub</a>
@@ -42,7 +42,7 @@ const Projects = () => {
 						</a>
 					<section>
 						<h3 className="project-title">WBWR</h3>
-						<p className="project-desctiption">Word by word reader is a menu bar application built in electron. Copy and paste a text block into the application and WBWR will display each word in the reader at the designated wpm. By displaying each word in the same place you are able to read at a faster pace.</p>
+						<p className="project-desctiption">Word by word reader is a menu bar application built in Electron. Copy and paste a text block into the application and WBWR will display each word in the reader at the designated wpm. By displaying each word in the same place you are able to read at a faster pace.</p>
 						<div className="project-links">
 							<a href="https://github.com/Obleo33/wbwr" target="_blank" rel="noopener noreferrer">GitHub</a>
 						</div>
