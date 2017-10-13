5d77db6f82fdca70886f191d46388c752e84cf1d
Franklin Crosby <obleo33@gmail.com>
2017-10-13 10:00:52 -0600

    Add contact info, links and styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,18 +3,19 @@ import React from 'react';
 const Contact = () => {
 	return(
 		<footer id="contact">
-			<div className="contact-container">
-				<p>Franklin Crosby</p>
-				<p>EMAIL: obleo33@gmail.com</p>
-				<p>PHONE: 303.887.2413</p>
-				<p>GIT: <a href="https://github.com/Obleo33">https://github.com/Obleo33</a></p>
-
-				<div className="source-link">
-					<p>Check out the source code for this page </p>
-					<a href="https://github.com/Obleo33/franklin-crosby">HERE</a>
-				</div>
-				<p>Built in React</p>
+			<h2>CONTACT</h2>
+			<p>lets collaborate <a href="mailto:obleo33@gmail.com">obleo33@gmail.com</a></p>
+			
+			<div className="contact-links">
+				<a href="https://github.com/Obleo33" target="_blank" rel="noopener noreferrer"><div className="contact-link git"></div></a>
+				<a href="https://www.linkedin.com/in/franklincrosby" target="_blank" rel="noopener noreferrer"><div className="contact-link linkedin"></div></a>
+				<a href="mailto:obleo33@gmail.com"><div className="contact-link email"></div></a>
 			</div>
+
+			<div className="source-link">
+				<a href="https://github.com/Obleo33/franklin-crosby" target="_blank" rel="noopener noreferrer">Check out the source code for this page</a>
+			</div>
+			<p>Built in React</p>
 		</footer>
 	)
 }
